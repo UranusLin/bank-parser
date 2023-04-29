5c9d30e3b66d04eeb44e588ab44fc736c4ef1889
MorrisLin <aka611511@gmail.com>
2023-04-29 16:09:03 +0800

    feat: add parser html to mongo db

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# This is a sample Python script.
-from service import financial_statement
+from service import financial_statement, read_form_html, insert_mongo_by_time, insert_mongo_by_bank
 
 if __name__ == '__main__':
-    print(financial_statement(2014, 2, 5843))
+    # print(financial_statement(2014, 2, 5843))
+    merge_df = read_form_html("./data/data_1.html")
+    # insert_mongo_by_time(merge_df)
+    # insert_mongo_by_bank(merge_df)
--- service.py
+++ service.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
-import numpy as np
+from pymongo import MongoClient
 
+client = MongoClient('mongodb://zeroPass:9674Ephzx&T7@127.0.0.1:27017/?retryWrites=true&w=majority')
 
 def financial_statement(year, season, com_code):
     if year >= 1000:
@@ -25,3 +26,41 @@ def financial_statement(year, season, com_code):
     # return pd.concat(dfs[1:], axis=0, sort=False)\
     #          .set_index(['公司代號'])\
     #          .apply(lambda s: pd.to_numeric(s, errors='ceorce'))
+
+
+def read_form_html(path: str):
+    dfs = pd.read_html(path, header=None)
+    merged_df = pd.merge(dfs[1], dfs[2], on=['跨類別查詢', '跨類別查詢.1'], how='inner')
+    # rename columns name
+    merged_df = merged_df.rename(columns={'跨類別查詢': '時間', '跨類別查詢.1': '銀行'})
+    return merged_df
+
+
+def insert_mongo_by_time(merged_df):
+    db = client['time_series']
+
+    for index, row in merged_df.iterrows():
+        row_dict = row.to_dict()
+        # store collection by 時間
+        temp = row_dict['時間'].split(' ')
+        collection = db[temp[0]]
+        if len(temp) > 1:
+            if temp[1] in ['3月', '6月', '9月', '12月']:
+                # insert data
+                collection.insert_one(row_dict)
+
+def insert_mongo_by_bank(merge_df):
+    db = client['bank']
+    store_list = []
+    for index, row in merge_df.iterrows():
+        # init key by 銀行 and value is list
+        if row['銀行'] not in store_list:
+            store_list.append(row['銀行'])
+        # store collection by 銀行
+        collection = db[row['銀行']]
+        # insert data
+        temp = row['時間'].split(' ')
+        if len(temp) > 1:
+            if temp[1] in ['3月', '6月', '9月', '12月']:
+                # insert data
+                collection.insert_one(row.to_dict())
